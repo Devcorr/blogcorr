95830b0d4e7ec38f4f3e40592ddfa9c54a32eea5
Matt Bargar <mbargar@gmail.com>
2014-10-03 13:40:01 -0400

    gulp build somewhat working

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,49 @@
+var source = require('vinyl-source-stream');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var watchify = require('watchify');
+var notify = require("gulp-notify");
 
-gulp.task('default', function() {
-    // place code for your default task here
+var scriptsDir = './js';
+var buildDir = './js';
+
+
+function handleErrors() {
+    var args = Array.prototype.slice.call(arguments);
+    notify.onError({
+        title: "Compile Error",
+        message: "<%= error.message %>"
+    }).apply(this, args);
+    this.emit('end'); // Keep gulp from hanging on this task
+}
+
+
+// Based on: http://blog.avisi.nl/2014/04/25/how-to-keep-a-fast-build-with-browserify-and-reactjs/
+function buildScript(file, watch) {
+    var props = {entries: [scriptsDir + '/' + file]};
+    var bundler = watch ? watchify(props) : browserify(props);
+    bundler.transform(reactify);
+    function rebundle() {
+        var stream = bundler.bundle({debug: true});
+        return stream.on('error', handleErrors)
+            .pipe(source("bundle.js"))
+            .pipe(gulp.dest(buildDir + '/'));
+    }
+    bundler.on('update', function() {
+        rebundle();
+        gutil.log('Rebundle...');
+    });
+    return rebundle();
+}
+
+
+gulp.task('build', function() {
+    return buildScript('app.js', false);
+});
+
+
+gulp.task('default', ['build'], function() {
+    return buildScript('app.js', true);
 });
