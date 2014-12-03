8a1a434582f0e0ab4a52911342a8417c4ee7ea13
Matt Bargar <matt@devcorr.com>
2014-12-03 09:48:59 -0500

    Add author field to schema setup for blog posts

--- setup/initNewApp.js
+++ setup/initNewApp.js
@@ -4,39 +4,6 @@ var config = require('../config/localConfig.json');
 
 Parse.initialize(config.parseApplicationKey, config.parseJSKey);
 
-var BlogPost = Parse.Object.extend("BlogPost");
-
-// Initilize BlogPost class and data
-var firstPostQuery = new Parse.Query(BlogPost);
-firstPostQuery.equalTo("title", "First Post!");
-
-firstPostQuery.find({
-    success: function(results) {
-        var blogPost;
-
-        if (results.length) {
-            console.log("First post already initialized, skipping.");
-        } else {
-            blogPost = new BlogPost();
-            blogPost.set("title", "First Post!");
-            blogPost.set("text", "We are a software development shop and boutique consulting firm specializing in web and mobile applications. Devcorr leverages decades worth of combined development experience to deliver custom software solutions.");
-
-            blogPost.save(null, {
-                success: function(blogPost) {
-                    console.log("blogPost created successfully: " + blogPost.id);
-                },
-                error: function(blogPost, error) {
-                    console.log("Failed to create new blogPost, with error code: " + error.message);
-                }
-            });
-        }
-    },
-    error: function(error) {
-        alert("Error: " + error.code + " " + error.message);
-    }
-});
-
-// End BlogPost initialization
 
 // User setup
 
@@ -141,3 +108,47 @@ var initRoles = function () {
     });
 };
 // End role setup
+
+// Initilize BlogPost class and data
+Parse.User.logIn("Matt", "test", {
+    success: function (user) {
+        var BlogPost = Parse.Object.extend("BlogPost");
+
+        var firstPostQuery = new Parse.Query(BlogPost);
+        firstPostQuery.equalTo("title", "First Post!");
+
+        firstPostQuery.find({
+            success: function(results) {
+                var blogPost;
+
+                if (results.length) {
+                    console.log("First post already initialized, skipping.");
+                } else {
+                    blogPost = new BlogPost();
+                    blogPost.set("title", "First Post!");
+                    blogPost.set("text", "We are a software development shop and boutique consulting firm specializing in web and mobile applications. Devcorr leverages decades worth of combined development experience to deliver custom software solutions.");
+                    blogPost.set("author", user);
+
+                    blogPost.save(null, {
+                        success: function(blogPost) {
+                            console.log("blogPost created successfully: " + blogPost.id);
+                        },
+                        error: function(blogPost, error) {
+                            console.log("Failed to create new blogPost, with error code: " + error.message);
+                        }
+                    });
+                }
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    },
+    error: function(user, error) {
+        alert("Error: " + error.code + " " + error.message);
+    }
+});
+
+
+// End BlogPost initialization
+
