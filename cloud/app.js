315c6aa3341590de470015b0431a77ec2069239d
Matt Bargar <matt@devcorr.com>
2014-12-03 14:16:52 -0500

    added route to express for redirects from old tumblr urls

--- cloud/app.js
+++ cloud/app.js
@@ -13,6 +13,28 @@ app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
 
+app.get('/post/:id/:name', function(req, res) {
+    var query;
+    var mappings = {
+        'why-i-cant-sleep': "Why I Can’t Sleep",
+        'meet-devcorr': "Meet Devcorr"
+    };
+
+    if (mappings.hasOwnProperty(req.params.name)) {
+        query = new Parse.Query("BlogPost");
+        query.equalTo("title", mappings[req.params.name]);
+        query.first({
+            success: function(post) {
+                res.redirect(301, '/posts/' + post.id);
+            },
+            error: function(error) {
+                console.error("Got an error " + error.code + " : " + error.message);
+            }
+        })
+    }
+
+});
+
 app.get('/*', function(req, res) {
     res.render('index');
 });
