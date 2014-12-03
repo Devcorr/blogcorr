265aa0dc2573db239592b844ecded4fb463878f5
Matt Bargar <matt@devcorr.com>
2014-12-03 16:12:31 -0500

    setting up all the tumblr redirects

--- cloud/app.js
+++ cloud/app.js
@@ -16,8 +16,16 @@ app.use(express.bodyParser());    // Middleware for reading request body
 app.get('/post/:id/:name', function(req, res) {
     var query;
     var mappings = {
-        'why-i-cant-sleep': "Why I Can’t Sleep",
-        'meet-devcorr': "Meet Devcorr"
+        'why-i-cant-sleep': "Why I Can't Sleep",
+        'meet-devcorr': "Meet Devcorr",
+        'its-just-an-mvp': "It's Just an MVP",
+        'try-fail-learn-repeat': "Try. Fail. Learn. Repeat.",
+        'testing-with-cucumber-in-the-java-ecosystem': "Testing with Cucumber in the Java Ecosystem",
+        'git-commits-on-osx-there-was-a-problem-with-the': "Git Commits on OSX: There was a problem with the editor 'vi'",
+        'adventures-in-appleland-xcode-5': "Adventures in Appleland: xcode 5",
+        'compiling-sass-or-less-on-the-fly-with-intellij': "Compiling Sass (or LESS) on the fly with IntelliJ",
+        '2013': "2013 { … }",
+        'cyclecorr-2014': "Cyclecorr 2014"
     };
 
     if (mappings.hasOwnProperty(req.params.name)) {
